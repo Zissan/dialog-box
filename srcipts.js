8246c23ec43ddb0daee4f456143a3a9d3f8a5e6a
zissan.chowdhury@yahoo.com <zissan.chowdhury@yahoo.com>
2021-10-02 10:21:57 +0530

    added logic to create modal using js

--- srcipts.js
+++ srcipts.js
@@ -1,4 +1,38 @@
 let modal = null;
-function createModal() {}
+let modalOverlay = null;
+const btnModal = document.getElementById("btnModal");
 
-function disposeModal() {}
+function createModal() {
+  modal = document.createElement("div");
+  modal.className = "modal modal--sm";
+  // NOTE: Using innerHTML is vulnerable to security attacks
+  // Prefer using template engine
+  modal.innerHTML = `
+        <div class="modal__header">Header</div>
+        <div class="modal__body">Body</div>
+        <div class="modal__footer">Footer</div>
+    `;
+
+  modalOverlay = document.createElement("div");
+  modalOverlay.className = "modal-overlay";
+
+  document.body.append(modalOverlay);
+  document.body.append(modal);
+}
+
+function disposeModal() {
+  if (modal) {
+    modal.remove();
+    modal = null;
+  }
+  if (modalOverlay) {
+    modalOverlay.remove();
+    modalOverlay = null;
+  }
+}
+
+btnModal.addEventListener("click", () => {
+  if (!modal && !modalOverlay) {
+    createModal();
+  }
+});
